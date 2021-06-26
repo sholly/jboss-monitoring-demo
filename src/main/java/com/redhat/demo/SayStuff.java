f9040aa81437fce8cf7fedd15ad6866b1bea2580
Jay Shollenberger <jshollen@redhat.com>
2021-06-26 14:13:13 -0600

    logging

--- src/main/java/com/redhat/demo/SayStuff.java
+++ src/main/java/com/redhat/demo/SayStuff.java
@@ -14,6 +14,8 @@ public class SayStuff {
     @Path("/saystuff")
     @Produces(MediaType.TEXT_PLAIN)
     public String sayStuff() {
+
+        System.out.println("In Say Stuff...");
         return "stuff";
     }
 }
